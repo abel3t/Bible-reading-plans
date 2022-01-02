a51664d05749d4a20b6bd598e16b540ebfac09bf
Abel Tran <abeltran.develop@gmail.com>
2022-01-02 16:50:09 +0700

    Add data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,59 @@
-import { Slider } from "@mui/material";
+import { Box } from "@mui/material";
 import type { NextPage } from "next";
+import { format, differenceInDays } from "date-fns";
+import {
+  dailyGospel,
+  dailyPsalm,
+  dailyProverbs,
+  dailyBible,
+  dailyActs,
+} from "../constant";
 
 const Home: NextPage = () => {
+  const day = differenceInDays(
+    new Date("January 1, 2023"),
+    new Date("January 1, 2022")
+  );
+
+  const plans = {
+    gospel: dailyGospel[day % dailyGospel.length],
+    psalms: dailyPsalm[day % dailyPsalm.length],
+    proverbs: dailyProverbs[day % dailyProverbs.length],
+    bible: dailyBible[day % dailyBible.length],
+    acts: dailyActs[day % dailyActs.length],
+  };
+
   return (
     <>
-      <Slider aria-label="Volume" value={1} onChange={() => console.log("x")} />
+      <Box>
+        <p>DAILY BIBLE READING</p>
+
+        <div>
+          <p>{format(new Date(), "PPP")}</p>
+
+          <div>
+            <p>
+              <span>Gospel: </span>
+              <span>{plans.gospel}</span>
+            </p>
+
+            <p>
+              <span>Psalms: </span>
+              <span>{plans.psalms}</span>
+            </p>
+
+            <p>
+              <span>Proverbs: </span>
+              <span>{plans.proverbs}</span>
+            </p>
+
+            <p>
+              <span>Acts: </span>
+              <span>{plans.acts}</span>
+            </p>
+          </div>
+        </div>
+      </Box>
     </>
   );
 };
