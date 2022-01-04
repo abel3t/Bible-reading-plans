a7ee99798dac39bd4735693bb595159faf4e16b5
Abel Tran <abeltran.develop@gmail.com>
2022-01-04 07:50:11 +0700

    Update resposive

--- src/pages/layouts/header.tsx
+++ src/pages/layouts/header.tsx
@@ -53,9 +53,9 @@ const Header: React.FC = () => {
   };
 
   return (
-      <div className="flex content-center xs:3/4 md:w-2/3 lg:w-1/2 border-solid border-b-2 p-2"
+      <div className="flex content-center items-center xs:3/4 md:w-2/3 lg:w-1/2 border-solid border-b-2 p-2"
            style={{ margin: '0 auto', borderColor: '#3E6F92' }}>
-        <div className="text-2xl">DAILY BIBLE READING</div>
+        <div className="text-md md:text-2xl">DAILY BIBLE READING</div>
         <div className="grow flex justify-end">
           <Modal
               aria-labelledby="transition-modal-title"
